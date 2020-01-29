067586f78c33e2da0582781a29871b20da1d2f46
Norberto Santiago <norbertosantiago82@gmail.com>
2020-01-29 16:42:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,5 +7,12 @@ def reverse_each_word(string)
   return_array.join(" ")
   end
   
-  
+  def reverse_each_word(string)
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do |string|
+    return_array << string.reverse 
+  end
+  return_array.join(" ")
+  end
   reverse_each_word("Hello there, and how are you?")
